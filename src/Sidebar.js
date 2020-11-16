9d755605e6d7e75843e8ae9e033fcb57c01dfa7a
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-16 17:11:29 -0300

    sidebar style

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import './Sidebar.css';
 
+//icons 
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Avatar, IconButton} from '@material-ui/core';
+
 function Sidebar() {
   return(
       <div className="sidebar">
-          <h1>I am a sidebar</h1>
           <div className="sidebar__header">
+              <Avatar  src="https://avatars2.githubusercontent.com/u/45006908?s=460&u=886c48b82f23b3ff23ede6b03b729a9ad97e803e&v=4"/>
               <div className="sidebar__headerRight">
-                  
+                  <IconButton>
+                    <DonutLargeIcon />
+                  </IconButton>
+
+                  <IconButton>
+                    <ChatIcon />
+                  </IconButton>
+
+                  <IconButton>
+                    <MoreVertIcon />
+                  </IconButton>
               </div>
           </div>
       </div>
