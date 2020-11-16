2368fbfd62445af571dd19459868f7f052a03231
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-16 20:32:27 -0300

    chat header

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './Chat.css';
 
+//material ui
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+
+
 function Chat() {
   return( 
     <div className="chat">
-        <h1>I am the chat</h1>
+        <div className="chat__header">
+            <Avatar />
+
+            <div className="chat__headerInfo">
+                <h3>Room Name</h3>
+                <p>Last seen at...</p>
+            </div>
+
+            <div className="chat__headerRight">
+                <IconButton>
+                    <SearchOutlined />
+                </IconButton>
+
+                <IconButton>
+                    <AttachFile />
+                </IconButton>
+
+                <IconButton>
+                    <MoreVert />
+                </IconButton>
+            </div>
+        </div>
+
+        <div className="chat__body">
+            
+        </div>
     </div>
   )
 }
