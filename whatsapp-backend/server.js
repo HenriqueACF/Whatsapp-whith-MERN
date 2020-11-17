04dfa98eb0e68f9d376e2ddf169659458dd5e3ee
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-17 20:40:28 -0300

    add pusher

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,13 +1,22 @@
 //importing
 import express from 'express';
 import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
 
 //app config
 const app = express();
 const port = process.env.PORT ||9000
 
-//middleware
+const pusher = new Pusher({
+    appId: "1108883",
+    key: "a582f519bac68a7650ad",
+    secret: "4e8573d569e1fe4c9042",
+    cluster: "us2",
+    useTLS: true
+  });
 
+//middleware
+app.use(express.json())
 
 //DB config
 const connection_url = 'mongodb+srv://admin:admin@cluster0.ldkms.mongodb.net/test';
@@ -21,6 +30,28 @@ mongoose.connect(connection_url,{
 
 //api routes
 app.get('/', (req, res) => res.status(200).send('Hello World'));
+
+app.get('/messages/sync', (req, res)=>{
+    Messages.find((err, data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+app.post('/messages/new', (req, res) =>{
+    const dbMessage = req.body
+
+    Messages.create(dbMessage, (err, data) => {
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
 
 //listen
 app.listen(port, () =>console.log(`LISTENING ON LOCALHOST:${port}`));
