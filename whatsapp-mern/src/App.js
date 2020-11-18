18417241a74581cb748398ab45bea56a3d5bd24c
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-18 15:16:59 -0300

    install pusher in front

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import Pusher from 'pusher-js';
 
 //Components
 import SideBar from './Sidebar';
 import Chat from './Chat';
 
+
+
 function App() {
+
+  useEffect(()=>{
+    const pusher = new Pusher('a582f519bac68a7650ad', {
+      cluster: 'us2'
+    });
+
+    const channel = pusher.subscribe('my-channel');
+    channel.bind('inserted', (data)=> {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+
   return (
     <div className="App">
       <div className="app__body">
