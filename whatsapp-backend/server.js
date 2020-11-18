9d0ef3ba9c2e3f560ae1ac891d3571a20f9aa8eb
Henrique Assis <henrique_assis_10@hotmail.com>
2020-11-18 14:56:03 -0300

    add middleware

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -19,6 +19,12 @@ const pusher = new Pusher({
 //middleware
 app.use(express.json())
 
+app.use((req, res, next)=>{
+    res.setHeader('Acess-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+})
+
 //DB config
 const connection_url = 'mongodb+srv://admin:admin@cluster0.ldkms.mongodb.net/test';
 mongoose.connect(connection_url,{
